0766a9df98a5ffa8bb607a669444ec5281511cce
juliang2127 <164977539+juliang2127@users.noreply.github.com>
2024-05-09 05:43:18 +0000

    think i got the user forms to work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,24 @@ class UsersController < ApplicationController
   end
 
   def show
-  url_username = params.fetch("path_username")
+    url_username = params.fetch("path_username")
     matching_usernames = User.where(:username => url_username)
     @the_user = matching_usernames.at(0)
     render ({ :template => "user_templates/show"})
   end
+
+  def create
+    @new_user = User.new
+    @new_user.username = params.fetch("input_username")
+    @new_user.save
+
+    redirect_to("/users/#{@new_user.username}")
+  end
+
+  def update
+    the_user = params.fetch("input_username")
+    @updated_user = User.where({:username = > the_user}).at(0)
+    @new_username = @updated_user.username
+    @new_username.save
+    redirect_to("/users/#{@new_user.username}")
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-
+  get("/", { :controller => "users", :action => "index" })
   #users
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show" } )
+  post("/add_user", { :controller => "users", :action => "create" }) 
+  post("/update_user", { :controller => "users", :action => "update" }) 
 
   # photos
   #get("/users", {})
