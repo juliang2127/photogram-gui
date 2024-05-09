d20af282fd0088552a4a431110ef312a26a8deec
juliang2127 <164977539+juliang2127@users.noreply.github.com>
2024-05-09 05:14:29 +0000

    users all done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+  #users
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:path_username", { :controller => "users", :action => "show" } )
+
+  # photos
+  #get("/users", {})
 end
